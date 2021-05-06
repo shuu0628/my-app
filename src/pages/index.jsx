367f0aad71385574dee201177628babae32926b9
shuu0628 <smasuda02@gmail.com>
2021-05-06 19:24:26 +0900

    ✨ useEffectの導入

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,18 +3,30 @@ import styles from "src/styles/Home.module.css";
 import { Footer } from "src/components/Footer";
 import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
-import { useCallback } from "react";
+import { useCallback, useEffect } from "react";
 
 
 export default function Home() {
-  const foo = 1;
+  // const foo = 1;
   
-  const handleClick = useCallback((e) => {
-    console.log(e.target.href);
-    e.preventDefault();
-    alert(foo);
-  }, []);
+  // const handleClick = useCallback((e) => {
+  //   console.log(e.target.href);
+  //   e.preventDefault();
+  //   alert(foo);
+  // }, []);
 
+  useEffect(() => {
+    console.log("マウント時");
+    //マウント時の処理
+    document.body.style.backgroundColor="lightblue";
+    
+    //アンマウント時の処理
+    return () => {
+      console.log("アンマウント時");
+      document.body.style.backgroundColor="";
+    }
+  }, [])
+  
   return (
     <div className={styles.container}>
       <Head>
