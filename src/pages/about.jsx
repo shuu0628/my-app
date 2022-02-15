5a2059c32020c337a04deaef1660bbe63f34e682
shuu0628 <smasuda02@gmail.com>
2022-02-15 22:34:38 +0900

    ✨ useMemo, useRouter

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -4,13 +4,32 @@ import { Header } from "src/components/Header";
 import { Main } from "src/components/Main";
 import styles from "src/styles/Home.module.css";
 
-export default function Home() {
+export default function About({
+  count,
+  isShow,
+  handleClick,
+  handleDisplay,
+  text,
+  array,
+  handleChange,
+  handleAdd,
+}) {
   return (
     <div className={styles.container}>
       <Head>
         <title>About Page</title>
       </Head>
       <Header />
+      {isShow ? <h1>{count}</h1> : null}
+      <button onClick={handleClick}>ボタン</button>
+      <button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
+      <input type="text" value={text} onChange={handleChange} />
+      <button onClick={handleAdd}>追加</button>
+      <ul>
+        {array?.map((item) => {
+          return <li key={item}>{item}</li>;
+        })}
+      </ul>
       <Main page="about" />
       <Footer />
     </div>
